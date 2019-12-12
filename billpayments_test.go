9174f1394d0ed24e0e719426a815d66ce802d7e9
Tokyo <rafaelgurudesign@gmail.com>
2019-12-11 21:31:37 -0300

    fix testing get Payment

--- billpayments_test.go
+++ billpayments_test.go
@@ -1,6 +1,7 @@
 package celcoin_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -96,7 +97,58 @@ func TestBillPaymentsAuthorize(t *testing.T) {
 func TestGetBillPayments(t *testing.T) {
 	godotenv.Load(".env.test")
 	client := celcoin.NewCelcoinClient(os.Getenv("CELCOIN_USER"), os.Getenv("CELCOIN_PASSWORD"), os.Getenv("ENV"))
-	paymentResponse, errAPI, err := client.GetBillPayments(os.Getenv("CELCOIN_TRANSACTIONID"))
+	barcode := celcoin.BarCode{
+		Type:      0,
+		Digitable: os.Getenv("CELCOIN_TEST_BOLETO_DIGITABLE"),
+		BarCode:   os.Getenv("CELCOIN_TEST_BOLETO_BARCODE"),
+	}
+	billPaymentsAuthorize := celcoin.BillPaymentAuthorize{
+		ExternalNSU:      0,
+		ExternalTerminal: "1111",
+		BarCode:          barcode,
+	}
+	payAuthorizeResponse, errAPI, err := client.BillPaymentsAuthorize(billPaymentsAuthorize)
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if payAuthorizeResponse == nil {
+		t.Error("payResponse is null")
+		return
+	}
+	barcode.Type = payAuthorizeResponse.Type
+	billpayments := celcoin.BillPayment{
+		ExternalNSU:      0,
+		ExternalTerminal: "1111",
+		Cpfcnpj:          os.Getenv("CELCOIN_TEST_BOLETO_DOCUMENT"),
+		BillData: celcoin.BillData{
+			Value:               10,
+			OriginalValue:       10,
+			ValueWithDiscount:   0,
+			ValueWithAdditional: 0,
+		},
+		BarCode:                barcode,
+		DueDate:                payAuthorizeResponse.DueDate,
+		TransactionIDAuthorize: payAuthorizeResponse.TransactionID,
+	}
+	payResponse, errAPI, err := client.BillPayments(billpayments)
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if payResponse == nil {
+		t.Error("payResponse is null")
+		return
+	}
+	paymentResponse, errAPI, err := client.GetBillPayments(fmt.Sprintf("%d", payResponse.TransactionID))
 	if err != nil {
 		t.Errorf("err : %s", err)
 		return

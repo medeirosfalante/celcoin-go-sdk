a600f6a3e6ad67f01ab6615246e8650c0fc6881f
Rafael Medeiros <rafaelgurudesign@gmail.com>
2020-06-19 22:59:08 -0300

    get Transaction

--- boleto_test.go
+++ boleto_test.go
@@ -1,6 +1,7 @@
 package celcoin_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -48,4 +49,59 @@ func TestBankslip(t *testing.T) {
 		t.Error("payResponse is null")
 		return
 	}
+}
+
+func TestGetTransactionStatus(t *testing.T) {
+	godotenv.Load(".env.test")
+	client := celcoin.NewCelcoinClient(os.Getenv("CELCOIN_USER"), os.Getenv("CELCOIN_PASSWORD"), os.Getenv("ENV"))
+	bankslipRequest := celcoin.BankslipRequest{
+		Values: celcoin.BankslipValues{
+			OriginalValue: 100,
+		},
+		Payer: celcoin.BankslipPayer{
+			Name:         "Ryan Cauã Felipe Araújo",
+			DocumentType: "CPF",
+			Document:     "80213959828",
+			Address:      "Rua Francisco Alves",
+			District:     "Jardim Sônia Maria",
+			City:         "Mauá",
+			UF:           "SP",
+			ZipCode:      "09380360",
+			Email:        "test@gmail.com",
+			Ddd:          "11",
+			PhoneNumber:  "982245864",
+		},
+		DueDate:      time.Now().AddDate(0, 0, 3).Format(time.RFC3339),
+		DaysToExpire: 3,
+		Instructions: celcoin.BankslipInstructionsRequest{
+			Instruction1: "Teste",
+		},
+	}
+	payAuthorizeResponse, errAPI, err := client.Bankslip(bankslipRequest)
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if payAuthorizeResponse == nil {
+		t.Error("payResponse is null")
+		return
+	}
+	transactionResponse, errAPI, err := client.GetTransactionStatus(fmt.Sprintf("%d", payAuthorizeResponse.TransactionID))
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	if errAPI != nil {
+		t.Errorf("errAPI : %#v", errAPI)
+		return
+	}
+	if transactionResponse == nil {
+		t.Error("payResponse is null")
+		return
+	}
+
 }
